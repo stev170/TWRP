a4388abc60fda039d989b59ae2d30b28394822a2
Stev_Dev <74199611+stev170@users.noreply.github.com>
2022-10-26 15:30:01 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # telegram
-export TG_CHAT_ID: -1001573823621
-export TG_TOKEN: ENCRYPTED[!98e054fa9aab8ca43b965ec07db0e84f1834205d4f40954a8f8134e6e951aadb00574466c6b82828eef4860da34925af!]
+#export TG_CHAT_ID: -1001573823621
+#export TG_TOKEN: ENCRYPTED[!98e054fa9aab8ca43b965ec07db0e84f1834205d4f40954a8f8134e6e951aadb00574466c6b82828eef4860da34925af!]
 
 # Device
 export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
